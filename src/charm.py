66d7b98b01b9574ba4badaa2177d44f894f20607
Carl Csaposs <carl.csaposs@canonical.com>
2024-03-15 11:41:54 +0100

    emit dummy event

--- src/charm.py
+++ src/charm.py
@@ -18,18 +18,29 @@ import ops
 
 logger = logging.getLogger(__name__)
 
+class FooEvent(ops.EventBase):
+    pass
+
 
 class FooCharm(ops.CharmBase):
     """Charm the service."""
+
+    foo_event = ops.EventSource(FooEvent)
 
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.update_status, self._on_update_status)
+        self.framework.observe(self.foo_event, self._on_foo)
 
     def _on_install(self, event: ops.InstallEvent):
+        logger.warning("A")
         event.defer()
+        self.foo_event.emit()
         return
+
+    def _on_foo(self, _):
+        pass
 
     def _on_update_status(self, event: ops.UpdateStatusEvent):
         logger.warning(f"{list(self.framework._storage.notices())=}")
